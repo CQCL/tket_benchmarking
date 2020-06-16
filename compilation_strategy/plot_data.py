e37373e0bdb61fc01bc46a90ff99e9b690f7a7ec
Alexander Cowtan <alexander.cowtan@cambridgequantum.com>
2020-06-16 14:03:51 +0100

    add results for vdbt

--- compilation_strategy/plot_data.py
+++ compilation_strategy/plot_data.py
@@ -3,47 +3,84 @@ import numpy as np
 import pandas
 import seaborn as sns
 
-for encoding in ('JW', 'BK', 'P'):
-    filename = '{}_results'.format(encoding)
+for encoding in ("JW", "BK", "P"):
+    filename = "{}_results".format(encoding)
 
-    table_initial = pandas.read_csv('{}.csv'.format(filename))
-    if encoding == 'JW':
-        table_TLOS = pandas.read_csv('TLOS_results.csv')
-    for metric in ('Count', 'Depth'):
+    table_initial = pandas.read_csv("{}.csv".format(filename))
+    if encoding == "JW":
+        table_TLOS = pandas.read_csv("TLOS_results.csv")
+        table_vdBT = pandas.read_csv("vdBT_results.csv")
+    for metric in ("Count", "Depth"):
 
-        list_spins = table_initial['Active Spin Orbitals']
-        list_uncoloured = table_initial['Naive CX {}'.format(metric)]
-        list_pair = table_initial['Pairwise CX {}'.format(metric)]
-        list_set = table_initial['Set CX {}'.format(metric)]
-        if encoding == 'JW':
-            list_template = table_TLOS['TLOS CX {}'.format(metric)]
-        name = 'CX {}'.format(metric)
+        list_spins = table_initial["Active Spin Orbitals"]
+        list_uncoloured = table_initial["Naive CX {}".format(metric)]
+        list_pair = table_initial["Pairwise CX {}".format(metric)]
+        list_set = table_initial["Set CX {}".format(metric)]
+        if encoding == "JW":
+            list_template = table_TLOS["TLOS CX {}".format(metric)]
+            if metric == "Count":
+                list_vdBT = table_vdBT["vdBT CX Count"]
+                list_spins_vdBT = table_vdBT["Active Spin Orbitals"]
+        name = "CX {}".format(metric)
 
-        all_lists = [list_uncoloured,list_pair,list_set]
-        if encoding == 'JW':
+        all_lists = [list_uncoloured, list_pair, list_set]
+        if encoding == "JW":
             all_lists.append(list_template)
+            if metric == "Count":
+                all_lists.append(list_vdBT)
 
         for l in all_lists:
-            reduction = 1-sum([l[i]/list_uncoloured[i] for i in range(len(list_uncoloured))])/len(list_uncoloured)
+            # ignore list_vdBT here
+            if len(l) != 19:
+                continue
+            reduction = 1 - sum(
+                [l[i] / list_uncoloured[i] for i in range(len(l))]
+            ) / len(l)
             print(reduction)
-        f, (ax1) = plt.subplots(1,1)
+        f, (ax1) = plt.subplots(1, 1)
 
-
-        markers = ['v','p','x','.']
-        colours = sns.color_palette('Set2')
-        strat = ['Naive','Pairwise','Sets','TLOS']
-        linestyles = [':','--','-.','-']
+        markers = ["v", "p", "x", ".", "<"]
+        colours = sns.color_palette("Set2")
+        strat = ["Naive", "Pairwise", "Sets", "TLOS", "vdBT"]
+        linestyles = [":", "--", "-.", "-", (0, (3, 1, 1, 1, 1, 1))]
         d = 4
-        for i, l in enumerate(all_lists) :
-            ax1.plot(list_spins,l,label=strat[i],marker=markers[i],markersize=6,c=colours[i],linewidth=0)
-            ax1.plot(list_spins, 10**(np.poly1d(np.polyfit(list_spins,np.log10(l), d))(list_spins)),linewidth=1.1,linestyle=linestyles[i],c=colours[i])
+        for i, l in enumerate(all_lists):
+            if i == 4:
+                list_spins = list_spins_vdBT
+            ax1.plot(
+                list_spins,
+                l,
+                label=strat[i],
+                marker=markers[i],
+                markersize=6,
+                c=colours[i],
+                linewidth=0,
+            )
+            ax1.plot(
+                list_spins,
+                10 ** (np.poly1d(np.polyfit(list_spins, np.log10(l), d))(list_spins)),
+                linewidth=1.1,
+                linestyle=linestyles[i],
+                c=colours[i],
+            )
 
-        ax1.set_yscale('log')
-        ax1.set_xlabel('Active Spin Orbitals')
-        ax1.set_ylabel('{}'.format(name))
+        ax1.set_yscale("log")
+        ax1.set_xlabel("Active Spin Orbitals")
+        ax1.set_ylabel("{}".format(name))
 
         handles, labels = ax1.get_legend_handles_labels()
-        plt.legend(handles=handles,labels=labels,loc='upper left',ncol=2,title='Strategy',fancybox=True,handlelength=1,markerscale=1.3)
-        f.set_size_inches(6,5,forward=True)
-        stat = name.replace(' ', '_')
-        plt.savefig('plots/Compare_{}_{}.eps'.format(encoding,stat), format='eps', dpi=1000)
+        plt.legend(
+            handles=handles,
+            labels=labels,
+            loc="upper left",
+            ncol=2,
+            title="Strategy",
+            fancybox=True,
+            handlelength=1,
+            markerscale=1.3,
+        )
+        f.set_size_inches(6, 5, forward=True)
+        stat = name.replace(" ", "_")
+        plt.savefig(
+            "plots/Compare_{}_{}.eps".format(encoding, stat), format="eps", dpi=1000
+        )
